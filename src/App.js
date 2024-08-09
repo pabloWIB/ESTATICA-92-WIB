474ad17fb08ef016af3df46810e36ef00d2b7ba6
Pablo <pablonietoperez369@gmail.com>
2024-08-09 02:46:42 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Spline from '@splinetool/react-spline';
+
 import './App.css';
+import './normalize.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <Spline scene="https://prod.spline.design/FpSkAf2BfXyfzMDP/scene.splinecode" />
-    </div>
+    <body>
+      <div className="App">
+        <Spline scene="https://prod.spline.design/hWFn4mygo3IaW4u7/scene.splinecode" />
+      </div>
+      <main>
+        <nav>
+          <div>HOME PAGE</div>
+          <div>TIKTOK</div>
+        </nav>
+        <footer>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </footer>
+      </main>
+      
+    </body>
   );
 }
-
